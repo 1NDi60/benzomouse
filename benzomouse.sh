3c50043f2f6553aac2b8c94a7e9de977ed9ee807
Indy <131899575+1NDi60@users.noreply.github.com>
2023-08-03 01:19:06 -0400

    Update benzomouse.sh
    fixed an issue where plists weren't being properly updated

--- benzomouse.sh
+++ benzomouse.sh
@@ -12,7 +12,17 @@ if grep -q "cursors $DEST_DIR" /etc/synthetic.conf; then
   cp -R "$SOURCE_DIR" "$DEST_DIR"
 
   # Iterate through all the plist files and modify hotx and hoty values
-  find "$DEST_DIR" -name "*.plist" -exec sed -i '' -E '/<key>hotx<\/key>/ {n; s/<integer>[0-9]+<\/integer>/<integer>4<\/integer>/;} /<key>hoty<\/key>/ {n; s/<integer>[0-9]+<\/integer>/<integer>4<\/integer>/;}' {} \;
+  find "$DEST_DIR" -name "*.plist" -exec sh -c '
+    # Convert plist to XML
+    plutil -convert xml1 {}
+
+    # Modify hotx and hoty values using a temporary file
+    xmlstarlet edit --update "/plist/dict/key[text()=\"hotx\"]/following-sibling::integer[1]" --value "4" --update "/plist/dict/key[text()=\"hoty\"]/following-sibling::integer[1]" --value "4" {} > {}.temp
+    mv {}.temp {}
+
+    # Convert back to binary plist
+    plutil -convert binary1 {}
+  ' sh {} \;
 
   echo "Done! Modifications have been made."
   
